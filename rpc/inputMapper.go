fa6f692dd930768f41bf3090ce0440c310c1ff8a
Victiniiiii <victinii494@gmail.com>
2025-09-14 16:34:35 +0300

    added Type:2 into the activity

--- rpc/inputMapper.go
+++ rpc/inputMapper.go
@@ -1,69 +1,5 @@
 package rpc
 
-import (
-	"time"
-)
-
-// Activity holds the data for discord rich presence
-type Activity struct {
-	// What the player is currently doing
-	Details string
-	// The user's current party status
-	State string
-	// The id for a large asset of the activity, usually a snowflake
-	LargeImage string
-	// Text displayed when hovering over the large image of the activity
-	LargeText string
-	// The id for a small asset of the activity, usually a snowflake
-	SmallImage string
-	// Text displayed when hovering over the small image of the activity
-	SmallText string
-	// Information for the current party of the player
-	Party *Party
-	// Unix timestamps for start and/or end of the game
-	Timestamps *Timestamps
-	// Secrets for Rich Presence joining and spectating
-	Secrets *Secrets
-	// Clickable buttons that open a URL in the browser
-	Buttons []*Button
-}
-
-// Button holds a label and the corresponding URL that is opened on press
-type Button struct {
-	// The label of the button
-	Label string
-	// The URL of the button
-	Url string
-}
-
-// Party holds information for the current party of the player
-type Party struct {
-	// The ID of the party
-	ID string
-	// Used to show the party's current size
-	Players int
-	// Used to show the party's maximum size
-	MaxPlayers int
-}
-
-// Timestamps holds unix timestamps for start and/or end of the game
-type Timestamps struct {
-	// unix time (in milliseconds) of when the activity started
-	Start *time.Time
-	// unix time (in milliseconds) of when the activity ends
-	End *time.Time
-}
-
-// Secrets holds secrets for Rich Presence joining and spectating
-type Secrets struct {
-	// The secret for a specific instanced match
-	Match string
-	// The secret for joining a party
-	Join string
-	// The secret for spectating a game
-	Spectate string
-}
-
 func mapActivity(activity *Activity) *PayloadActivity {
 	final := &PayloadActivity{
 		Details: activity.Details,
@@ -74,6 +10,7 @@ func mapActivity(activity *Activity) *PayloadActivity {
 			SmallImage: activity.SmallImage,
 			SmallText:  activity.SmallText,
 		},
+		Type: 2,
 	}
 
 	if activity.Timestamps != nil && activity.Timestamps.Start != nil {
--- rpc/types.go
+++ rpc/types.go
@@ -1,6 +1,10 @@
 package rpc
 
-import "github.com/fawni/rp/ipc"
+import (
+	"time"
+
+	"github.com/fawni/rp/ipc"
+)
 
 type Handshake struct {
 	V        string `json:"v"`
@@ -26,6 +30,7 @@ type PayloadActivity struct {
 	Timestamps *PayloadTimestamps `json:"timestamps,omitempty"`
 	Secrets    *PayloadSecrets    `json:"secrets,omitempty"`
 	Buttons    []*PayloadButton   `json:"buttons,omitempty"`
+	Type       int                `json:"type,omitempty"`
 }
 
 type PayloadAssets struct {
@@ -60,4 +65,64 @@ type Client struct {
 	ClientID string
 	IPC      *ipc.IPC
 	Logged   bool
+}
+
+// Activity holds the data for discord rich presence
+type Activity struct {
+	// What the player is currently doing
+	Details string
+	// The user's current party status
+	State string
+	// The id for a large asset of the activity, usually a snowflake
+	LargeImage string
+	// Text displayed when hovering over the large image of the activity
+	LargeText string
+	// The id for a small asset of the activity, usually a snowflake
+	SmallImage string
+	// Text displayed when hovering over the small image of the activity
+	SmallText string
+	// Information for the current party of the player
+	Party *Party
+	// Unix timestamps for start and/or end of the game
+	Timestamps *Timestamps
+	// Secrets for Rich Presence joining and spectating
+	Secrets *Secrets
+	// Clickable buttons that open a URL in the browser
+	Buttons []*Button
+}
+
+// Button holds a label and the corresponding URL that is opened on press
+type Button struct {
+	// The label of the button
+	Label string
+	// The URL of the button
+	Url string
+}
+
+// Party holds information for the current party of the player
+type Party struct {
+	// The ID of the party
+	ID string
+	// Used to show the party's current size
+	Players int
+	// Used to show the party's maximum size
+	MaxPlayers int
+}
+
+// Timestamps holds unix timestamps for start and/or end of the game
+type Timestamps struct {
+	// unix time (in milliseconds) of when the activity started
+	Start *time.Time
+	// unix time (in milliseconds) of when the activity ends
+	End *time.Time
+}
+
+// Secrets holds secrets for Rich Presence joining and spectating
+type Secrets struct {
+	// The secret for a specific instanced match
+	Match string
+	// The secret for joining a party
+	Join string
+	// The secret for spectating a game
+	Spectate string
 }
